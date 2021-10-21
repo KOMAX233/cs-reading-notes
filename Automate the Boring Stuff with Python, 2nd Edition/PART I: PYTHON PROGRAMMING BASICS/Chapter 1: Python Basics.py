8f9a31f87a3671f8f05f7e6065fdd0bfbd5f195c
KOMAX233 <55835642+YueFangOfficial@users.noreply.github.com>
2021-10-21 19:19:12 -0400

    Create Chapter 1: Python Basics.py

---
+++ Automate the Boring Stuff with Python, 2nd Edition/PART I: PYTHON PROGRAMMING BASICS/Chapter 1: Python Basics.py
@@ -0,0 +1,27 @@
+# expression = value + operator
+
+# data types: Integer, Floating-Point number, String
+  
+# + * for int, float / str
+
+# variable: save values
+  # assign
+  # overwrite
+  
+  
+# PRINT HELLO WORLD AND ASK FOR NAME
+print("hello world")
+print('what\'s your name?')
+my_name = input()
+print('hello' + my_name)
+print('the length of ' + my_name + ' is: ')
+print(len(my_name))
+print('how old are you?')
+my_age = input()
+print('you\'ll be ' + str(int(my_age) + 1) + ' next year!')
+
+# http://pythontutor.com/ python visualization
+
+# function(arguments)
+
+
