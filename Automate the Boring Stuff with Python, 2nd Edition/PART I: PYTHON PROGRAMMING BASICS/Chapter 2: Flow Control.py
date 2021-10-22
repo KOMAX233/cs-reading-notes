6e68a1830816a6ecd228be5759104148ce122bd2
KOMAX233 <55835642+YueFangOfficial@users.noreply.github.com>
2021-10-22 00:53:04 -0400

    Create Chapter 2: Flow Control.py

---
+++ Automate the Boring Stuff with Python, 2nd Edition/PART I: PYTHON PROGRAMMING BASICS/Chapter 2: Flow Control.py
@@ -0,0 +1,6 @@
+# Boolean values: True/False, comparison operators, Boolean operators: and/or/not
+
+# ==: check if equal, =: assignment
+
+# flow control = condition(boolean expression) + clause()
+
